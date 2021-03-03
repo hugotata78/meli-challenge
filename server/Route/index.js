252e7f75f77bf78b668aac77033172a15e90e1be
hugotata78 <carlitosyagus_@hotmail.com.ar>
2021-03-03 13:05:52 -0300

    mejoras en las rutas

--- server/Route/index.js
+++ server/Route/index.js
@@ -6,13 +6,13 @@ const { response } = require('express')
 router.get('/search',(req,res)=>{
     axios.get(`https://api.mercadolibre.com/sites/MLA/search?q=${req.query.q}`)
     .then(response=> res.json(response.data.results))
-    .catch(err=>console.error(err))
+    .catch(err=>res.send(err))
 })
 
 router.get('/items/:id',(req,res)=>{
     axios.get(`https://api.mercadolibre.com/items/${req.params.id}`)
     .then(response=> res.json(response.data))
-    .catch(err=>console.error(err))
+    .catch(err=>res.send(err))
 })
 
 module.exports = router
